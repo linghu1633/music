1993e5a9a75d85dbe18b2e96ffe5065df2e4cb5d
linghu1633 <44311914+linghu1633@users.noreply.github.com>
2020-12-16 23:17:17 +0800

    Initial commit

---
+++ src/main/java/com/java/music/music/MusicApplication.java
@@ -0,0 +1,18 @@
+package com.java.music.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 热加载，热更新
+ * ctrl shift --> 搜索registry  然后找到 compiler.outomake.allow.when.app.running
+ * ctrl + F9   执行快捷方式，热加载生效
+ */
+@SpringBootApplication
+public class MusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApplication.class, args);
+    }
+
+}
